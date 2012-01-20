b2faa24ad31a45f4a09b9db8dc24c3dfb350bbdb
joc4ntonio <jalopez@pucp.pe>
2012-01-19 19:05:08 -0500

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WatchingConsumer::Application.config.secret_token = '3d5b1378148650c5f5d2854650bfcb0a99212cf38af7ffdd2a728f6bce3173383a11ec1c316d1edd34defed146fac17474ea387790732325cb74384a76173f1b'
